a56e35df69d96ef08af0bceb925454e37de48fdc
Chen Qiu <qiu.chen1@northeastern.edu>
2022-01-16 19:24:19 -0800

    solve not working

--- Sudoku/solver.py
+++ Sudoku/solver.py
@@ -68,10 +68,10 @@ def solve(bo):
         if valid(bo, i, (row, col)):
             bo[row][col] = i
         
-        if solve(bo):
-            return True
-        
-        bo[row][col] = 0
+            if solve(bo):
+                return True
+            
+            bo[row][col] = 0
     
     return False
 
