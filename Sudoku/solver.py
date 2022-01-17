da20a05aa3c0aa0779a1d2b6d650a5efd64d12ee
Chen Qiu <qiu.chen1@northeastern.edu>
2022-01-16 19:32:36 -0800

    complete solver

--- Sudoku/solver.py
+++ Sudoku/solver.py
@@ -10,11 +10,29 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+
 def print_board(bo):
     for i in range(len(bo)):
         if i % 3 == 0 and i != 0:
             print("- - - - - - - - - - - - - ")
-        
+
         for j in range(len(bo[0])):
             if j % 3 == 0 and j != 0:
                 print(" | ", end="")
@@ -39,8 +57,8 @@ def valid(bo, num, pos):
     box_x = pos[1] // 3
     box_y = pos[0] // 3
 
-    for i in range(box_x*3, box_x*3 + 3):
-        for j in range(box_y*3, box_y*3 + 3):
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x*3, box_x*3 + 3):
             if bo[i][j] == num and (i, j) != pos:
                 return False
     
@@ -50,30 +68,11 @@ def find_empty(bo):
     for i in range(len(bo)):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:
-                return (i, j) # row, col
-    
+                return (i, j)  # row, col
+
     return None
 
 
-def solve(bo):
-    # base case
-    find = find_empty(bo)
-    if not find:
-        return True
-    else:
-        row, col = find
-    
-    # recursive    
-    for i in range(1, 10):
-        if valid(bo, i, (row, col)):
-            bo[row][col] = i
-        
-            if solve(bo):
-                return True
-            
-            bo[row][col] = 0
-    
-    return False
 
 
 
